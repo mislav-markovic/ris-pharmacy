1ff8c1ac5b0d80307c14571ee643e0c73111e407
Mislav Marković <mislav.markovic95@gmail.com@fer.h>
2019-04-27 19:43:32 +0200

    Converter methods

---
+++ Pharmacy/Pharmacy.DataAccessLayer/Converters/BusinessLayerConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DALModels = Pharmacy.DataAccessLayer.Models;
+using BLLModels = Pharmacy.BusinessLayer.Models;
+
+namespace Pharmacy.DataAccessLayer.Converters
+{
+  public static class BusinessLayerConverter
+  {
+    public static DALModels.Location ToDAL(this BLLModels.Location model)
+    {
+      return null;
+    }
+
+    public static DALModels.Medicine ToDAL(this BLLModels.Medicine model)
+    {
+      return null;
+    }
+
+    public static DALModels.Order ToDAL(this BLLModels.Order model)
+    {
+      return null;
+    }
+
+    public static DALModels.Pharmacy ToDAL(this BLLModels.Pharmacy model)
+    {
+      return null;
+    }
+
+    public static DALModels.Prescription ToDAL(this BLLModels.Prescription model)
+    {
+      return null;
+    }
+
+    public static DALModels.Stockpile ToDAL(this BLLModels.Stockpile model)
+    {
+      return null;
+    }
+
+    public static DALModels.User ToDAL(this BLLModels.User model)
+    {
+      return null;
+    }
+
+    public static DALModels.UserRole ToDAL(this BLLModels.UserRole model)
+    {
+      return null;
+    }
+
+    public static DALModels.Warehouse ToDAL(this BLLModels.Warehouse model)
+    {
+      return null;
+    }
+  }
+}
---
+++ Pharmacy/Pharmacy.DataAccessLayer/Converters/DataAccessLayerConverter.cs
@@ -0,0 +1,53 @@
+﻿using DALModels = Pharmacy.DataAccessLayer.Models;
+using BLLModels = Pharmacy.BusinessLayer.Models;
+
+namespace Pharmacy.DataAccessLayer.Converters
+{
+  public static class DataAccessLayerConverter
+  {
+    public static BLLModels.Location ToBLL(this DALModels.Location model)
+    {
+      return null;
+    }
+
+    public static BLLModels.Medicine ToBLL(this DALModels.Medicine model)
+    {
+      return null;
+    }
+
+    public static BLLModels.Order ToBLL(this DALModels.Order model)
+    {
+      return null;
+    }
+
+    public static BLLModels.Pharmacy ToBLL(this DALModels.Pharmacy model)
+    {
+      return null;
+    }
+
+    public static BLLModels.Prescription ToBLL(this DALModels.Prescription model)
+    {
+      return null;
+    }
+
+    public static BLLModels.Stockpile ToBLL(this DALModels.Stockpile model)
+    {
+      return null;
+    }
+
+    public static BLLModels.User ToBLL(this DALModels.User model)
+    {
+      return null;
+    }
+
+    public static BLLModels.UserRole ToBLL(this DALModels.UserRole model)
+    {
+      return null;
+    }
+
+    public static BLLModels.Warehouse ToBLL(this DALModels.Warehouse model)
+    {
+      return null;
+    }
+  }
+}
